0ac8185cd4ecb49d021267a8a2005da96998bf71
kamran saleem <freinds221@gmail.com>
2022-10-03 23:03:32 +0500

    in

---
+++ lib/pages/go_live_screen.dart
@@ -0,0 +1,112 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:twitch_clone/utilis/colors.dart';
+import 'package:twitch_clone/widgets/custom_button.dart';
+import 'package:twitch_clone/widgets/custom_textfield.dart';
+
+class GoLiveScreen extends StatefulWidget {
+  const GoLiveScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GoLiveScreen> createState() => _GoLiveScreenState();
+}
+
+class _GoLiveScreenState extends State<GoLiveScreen> {
+  final TextEditingController _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 22.0,
+                        vertical: 20.0,
+                      ),
+                      child: DottedBorder(
+                        borderType: BorderType.RRect,
+                        radius: const Radius.circular(10),
+                        dashPattern: const [10, 4],
+                        strokeCap: StrokeCap.round,
+                        color: buttonColor,
+                        child: Container(
+                          width: double.infinity,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            color: buttonColor.withOpacity(.05),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.folder_open,
+                                color: buttonColor,
+                                size: 40,
+                              ),
+                              const SizedBox(height: 15),
+                              Text(
+                                'Select your thumbnail',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.grey.shade400,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Title',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: CustomTextField(
+                          message: '',
+                          controller: _titleController,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 10,
+                ),
+                child: CustomButton(
+                  text: 'Go Live!',
+                  onTap: () {},
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
